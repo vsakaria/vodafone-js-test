c4e261202c6579c75a400dd1aba595cecac0fde2
Vishal Sakaria <vsakaria@gmail.com>
2020-04-13 13:29:48 +0100

    UPDATE main css

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from "react";
-import styles from "./styles/ClientSummary.module.css";
+import styles from "./styles/Main.module.css";
 
 const Main = (props: any): ReactElement => {
   return (
-    <section className={styles.clientSummaryDetails}>
+    <section className={styles.main}>
       <h1 className="primary-color">Vodafone Test</h1>
     </section>
   );
